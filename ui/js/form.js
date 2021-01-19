f7faceb46b1c1b733e3215959e0967e63e621900
Martin <mr.mmaurer+work@gmail.com>
2021-01-19 22:14:06 +0100

    WIP form handling

--- ui/js/form.js
+++ ui/js/form.js
@@ -1,7 +1,31 @@
 import {html, render} from 'lit-html';
 
+let serialNumber = ''
+let calculatedNumber = 1234;
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log('submit', event)
+}
+
 const formTemplate = () => html`
-  <p>Hello test</p>
+  <form action="" method="post" @submit=${handleSubmit}>
+    <div>
+      <label for="serialNumber">Serial number:</label>
+      <input
+        type="text"
+        id="serial-number"
+        name="serial-number"
+        placeholder="Serial number"
+      />
+    </div>
+    <div>
+      <button type="submit">Submit</button>
+    </div>
+    <div>
+      <code>Calculated Number: ${calculatedNumber}</code>
+    </div>
+  </form>
 `;
 
 export default formTemplate;
