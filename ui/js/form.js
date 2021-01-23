db16e522ffdf88ab2849e2a3bd40ed9b2278971a
Martin <mr.mmaurer+work@gmail.com>
2021-01-24 00:23:05 +0100

    Moved form back to separate file

--- ui/js/form.js
+++ ui/js/form.js
@@ -1,5 +1,44 @@
-import {html, render} from 'lit-html';
+import { html } from 'lit-html';
 
+let serialNumber = '';
+const calculatedNumber = 1234;
 
+function handleSubmit(event) {
+  event.preventDefault();
+
+  console.log('submit', event);
+}
+
+function handleInput(event, renderCB) {
+  const newInputValue = event.target.value;
+
+  serialNumber = newInputValue;
+
+  console.log('handleInput', newInputValue, serialNumber);
+
+  renderCB();
+}
+
+const formTemplate = (renderCB) => html`
+  <form action="" method="post" @submit=${(event) => handleSubmit(event, renderCB)}>
+    <h1>${serialNumber}</h1>
+    <div>
+      <label for="serialNumber">Serial number:</label>
+      <input
+        type="text"
+        id="serial-number"
+        name="serial-number"
+        placeholder="Serial number"
+        @input=${(event) => handleInput(event, renderCB)}
+      />
+    </div>
+    <div>
+      <button type="submit">Submit</button>
+    </div>
+    <div>
+      <code>Calculated Number: ${calculatedNumber}</code>
+    </div>
+  </form>
+`;
 
 export default formTemplate;
--- ui/js/main.js
+++ ui/js/main.js
@@ -1,51 +1,14 @@
-import {html, render} from 'lit-html';
+import { html, render } from 'lit-html';
 
-// import formTemplate from './form.js';
-
-let serialNumber = ''
-let calculatedNumber = 1234;
-
-function handleSubmit(event) {
-  event.preventDefault();
-
-  console.log('submit', event);
-}
-
-function handleInput(event) {
-  const newInputValue = event.target.value;
-
-  serialNumber = newInputValue;
-
-  console.log('handleInput', newInputValue, serialNumber)
-
-  render(pageTemplate(), document.body);
-}
-
-const formTemplate = (mauTest) => html`
-  <form action="" method="post" @submit=${(event) => handleSubmit(event)}>
-    <h1>${serialNumber} ${mauTest}</h1>
-    <div>
-      <label for="serialNumber">Serial number:</label>
-      <input
-        type="text"
-        id="serial-number"
-        name="serial-number"
-        placeholder="Serial number"
-        @input=${(event) => handleInput(event)}
-      />
-    </div>
-    <div>
-      <button type="submit">Submit</button>
-    </div>
-    <div>
-      <code>Calculated Number: ${calculatedNumber}</code>
-    </div>
-  </form>
-`;
+import formTemplate from './form';
 
 const pageTemplate = () => html`
   <h1>Poescho-Radio</h1>
-  ${formTemplate()}
+  ${formTemplate(renderPage)}
 `;
 
-render(pageTemplate(), document.body);
+function renderPage() {
+  render(pageTemplate(), document.body);
+}
+
+renderPage();
