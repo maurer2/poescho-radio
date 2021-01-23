f1125e3b1446b3aac8817b5981e0547bbf7f3018
Martin <mr.mmaurer+work@gmail.com>
2021-01-23 23:30:16 +0100

    Moved form into main and fixed reactivity

--- ui/js/form.js
+++ ui/js/form.js
@@ -1,43 +1,5 @@
 import {html, render} from 'lit-html';
 
-let serialNumber = ''
-let calculatedNumber = 1234;
 
-function handleSubmit(event) {
-  event.preventDefault();
-  console.log('submit', event);
-}
-
-function handleInput(event, cb) {
-  const newInputValue = event.target.value;
-
-  serialNumber = newInputValue;
-
-  console.log('handleInput', newInputValue, serialNumber)
-
-  cb()
-}
-
-const formTemplate = (mauTest, updateTemplate) => html`
-  <form action="" method="post" @submit=${(event) => handleSubmit(event)}>
-    <h1>${serialNumber} ${mauTest}</h1>
-    <div>
-      <label for="serialNumber">Serial number:</label>
-      <input
-        type="text"
-        id="serial-number"
-        name="serial-number"
-        placeholder="Serial number"
-        @input=${(event) => handleInput(event, updateTemplate)}
-      />
-    </div>
-    <div>
-      <button type="submit">Submit</button>
-    </div>
-    <div>
-      <code>Calculated Number: ${calculatedNumber}</code>
-    </div>
-  </form>
-`;
 
 export default formTemplate;
--- ui/js/main.js
+++ ui/js/main.js
@@ -1,14 +1,51 @@
 import {html, render} from 'lit-html';
 
-import formTemplate from './form.js';
+// import formTemplate from './form.js';
 
-const template = html`
-  <h1>Poescho-Radio</h1>
-  ${formTemplate('mau', updateTemplate)}
-`;
+let serialNumber = ''
+let calculatedNumber = 1234;
 
-function updateTemplate() {
-  render(template, document.body);
+function handleSubmit(event) {
+  event.preventDefault();
+
+  console.log('submit', event);
 }
 
-updateTemplate()
+function handleInput(event) {
+  const newInputValue = event.target.value;
+
+  serialNumber = newInputValue;
+
+  console.log('handleInput', newInputValue, serialNumber)
+
+  render(pageTemplate(), document.body);
+}
+
+const formTemplate = (mauTest) => html`
+  <form action="" method="post" @submit=${(event) => handleSubmit(event)}>
+    <h1>${serialNumber} ${mauTest}</h1>
+    <div>
+      <label for="serialNumber">Serial number:</label>
+      <input
+        type="text"
+        id="serial-number"
+        name="serial-number"
+        placeholder="Serial number"
+        @input=${(event) => handleInput(event)}
+      />
+    </div>
+    <div>
+      <button type="submit">Submit</button>
+    </div>
+    <div>
+      <code>Calculated Number: ${calculatedNumber}</code>
+    </div>
+  </form>
+`;
+
+const pageTemplate = () => html`
+  <h1>Poescho-Radio</h1>
+  ${formTemplate()}
+`;
+
+render(pageTemplate(), document.body);
