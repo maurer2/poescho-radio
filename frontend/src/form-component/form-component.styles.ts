3560e64c4b69fbeb89ae170a08a9b602f6843b1f
Martin <mr.mmaurer+work@gmail.com>
2021-01-30 01:30:17 +0100

    Added external styles

---
+++ frontend/src/form-component/form-component.styles.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/prefer-default-export */
+import { css } from 'lit-element';
+
+export const FormComponentStyles = css`
+  .form {
+    margin: auto;
+    padding: 1rem;
+    border: 1px solid black;
+    max-width: 320px;
+  }
+`;
--- frontend/src/form-component/form-component.ts
+++ frontend/src/form-component/form-component.ts
@@ -4,66 +4,68 @@
 /* eslint-disable import/prefer-default-export */
 
 import {
-  LitElement, html, customElement, property, css,
-  } from 'lit-element';
+  LitElement, html, customElement, property,
+} from 'lit-element';
 
-  @customElement('form-component')
-  export class FormComponent extends LitElement {
-    static styles = css`
-      :host {
-        display: block;
-      }
+ import { FormComponentStyles } from './form-component.styles';
+
+@customElement('form-component')
+export class FormComponent extends LitElement {
+  static get styles() {
+    return [
+      FormComponentStyles,
+    ];
+  }
+
+  @property({ type: String })
+  serialNumber = '';
+
+  @property({ type: Number })
+  calculatedNumber = 1234;
+
+  private handleSubmit(event: Event) {
+    event.preventDefault();
+
+    console.log('handle submit', event);
+  }
+
+  private handleInput(event: InputEvent) {
+    if (!event.currentTarget) {
+      return;
+    }
+    const newInputValue = (event.currentTarget as HTMLInputElement).value;
+
+    this.serialNumber = newInputValue;
+  }
+
+  render() {
+    return html`
+      <form class="form" action="" method="post" @submit=${(event: Event) => this.handleSubmit(event)}>
+        <code>Serial number: ${this.serialNumber}</code>
+        <div>
+          <label for="serialNumber">Serial number:</label>
+          <input
+            type="text"
+            id="serial-number"
+            name="serial-number"
+            placeholder="Serial number"
+            .value=${this.serialNumber}
+            @input=${(event: InputEvent) => this.handleInput(event)}
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+        <div>
+          <code>Calculated Number: ${this.calculatedNumber}</code>
+        </div>
+      </form>
     `;
-
-    @property({ type: String })
-    serialNumber = '';
-
-    @property({ type: Number })
-    calculatedNumber = 1234;
-
-    private handleSubmit(event: Event) {
-      event.preventDefault();
-
-      console.log('handle submit', event);
-    }
-
-    private handleInput(event: Event) {
-      if (!event.currentTarget) {
-        return;
-      }
-      const newInputValue = (event.currentTarget as HTMLInputElement).value;
-
-      this.serialNumber = newInputValue;
-    }
-
-    render() {
-      return html`
-        <form action="" method="post" @submit=${(event: Event) => this.handleSubmit(event)}>
-          <code>Serial number: ${this.serialNumber}</code>
-          <div>
-            <label for="serialNumber">Serial number:</label>
-            <input
-              type="text"
-              id="serial-number"
-              name="serial-number"
-              placeholder="Serial number"
-              .value=${this.serialNumber}
-              @input=${(event: Event) => this.handleInput(event)}
-            />
-          </div>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-          <div>
-            <code>Calculated Number: ${this.calculatedNumber}</code>
-          </div>
-        </form>
-      `;
-    }
   }
+}
 
-  declare global {
-    interface HTMLElementTagNameMap {
-      'form-component': FormComponent;
-    }
+declare global {
+  interface HTMLElementTagNameMap {
+    'form-component': FormComponent;
   }
+}
